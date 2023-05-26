e8f456a3b0c97ed9f916df68001700065a6bb434
Yann BLAUDIN DE THE <yann.blaudin-de-the@thalesaleniaspace.com>
2023-05-26 15:58:06 +0200

    basilisk mockup

---
+++ CHANGELOG.md
@@ -0,0 +1,26 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+<!-- insertion marker -->
+## [0.1.0](https://github.com/ydethe/basilisk_mockup/releases/tag/0.1.0) - 2023-05-26
+
+<small>[Compare with first commit](https://github.com/ydethe/basilisk_mockup/compare/0b488ad8d2a572a4432b6b91af1f7234d07414d8...0.1.0)</small>
+
+### Added
+
+- Added pdm.lock ([588bab6](https://github.com/ydethe/basilisk_mockup/commit/588bab61d74bafef2770745a1b3e66275b0eac0d) by Yann de The).
+- Added README ([0b488ad](https://github.com/ydethe/basilisk_mockup/commit/0b488ad8d2a572a4432b6b91af1f7234d07414d8) by Yann BLAUDIN DE THE).
+
+### Fixed
+
+- Fixed python3.7 reqs ([931ca32](https://github.com/ydethe/basilisk_mockup/commit/931ca32d3331813d69c8546f7d6429be59b62116) by Yann BLAUDIN DE THE).
+- Fixed coverage ([1e014b6](https://github.com/ydethe/basilisk_mockup/commit/1e014b6474d959104082856c643b965304665eff) by Yann BLAUDIN DE THE).
+
+### Removed
+
+- Removed workflow manual run ([aef4f25](https://github.com/ydethe/basilisk_mockup/commit/aef4f25f47e369644f1fe8b59006f9dd4e0a1666) by Yann BLAUDIN DE THE).
+
---
+++ build_wheel.py
@@ -0,0 +1,118 @@
+# build_wheel.py
+
+import platform
+import os
+from pathlib import Path
+import zipfile
+import shutil
+
+from setuptools import Extension, Distribution
+from wheel.cli.convert import convert
+
+
+def zipdir(path: Path, ziph: zipfile.ZipFile):
+    """Add a directory to a zip file
+
+    Args:
+        path: Path to the directory to add
+        ziph: Opened zip file
+
+    From https://stackoverflow.com/a/1855118
+
+    """
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(
+                os.path.join(root, file),
+                os.path.relpath(os.path.join(root, file), os.path.join(path, "..")),
+            )
+
+
+def get_basilik_wheel_name() -> str:
+    """Get the right wheel name for Basilisk
+    Example: Basilisk_Sim-2.2.0b0-cp310-cp310-linux_x86_64.whl
+
+    From https://stackoverflow.com/a/60773383
+
+    Returns:
+        The wheel name
+
+    """
+    with open("docs/source/bskVersion.txt", "r") as f:
+        version = f.read().strip()
+
+    kwargs = dict(
+        name="Basilisk_Sim",
+        version=version,
+        ext_modules=[Extension("stub", sources=[])],
+    )
+
+    # create a fake distribution from arguments
+    dist = Distribution(attrs=kwargs)
+
+    # finalize bdist_wheel command
+    bdist_wheel_cmd = dist.get_command_obj("bdist_wheel")
+    bdist_wheel_cmd.ensure_finalized()
+
+    # assemble wheel file name
+    distname = bdist_wheel_cmd.wheel_dist_name
+    tag = "-".join(bdist_wheel_cmd.get_tag())
+
+    return f"{distname}-{tag}.whl"
+
+
+def build():
+    """Build a wheel file. 3 steps :
+
+    * Determine version strings and file names
+    * Build a egg file
+    * Build a wheel file
+
+    """
+    # ==========================================
+    # Determine some constants
+    # ==========================================
+    # Determine the current python version
+    vt = platform.python_version_tuple()
+    py_version = ".".join(vt[:-1])
+
+    # Determine Basilisk version
+    with open("docs/source/bskVersion.txt", "r") as f:
+        version = f.read().strip()
+
+    # Determine wheel and egg file names
+    whl_name = get_basilik_wheel_name()
+    egg_name = f"Basilisk_Sim-{version}-py{py_version}.egg"
+
+    build_pth = Path("dist3").expanduser().resolve()
+    root_pth = Path(os.getcwd()).expanduser().resolve()
+
+    # ==========================================
+    # egg file creation
+    # ==========================================
+    os.chdir(build_pth)
+
+    # Remove former EFF-INFO folder
+    shutil.rmtree("EGG-INFO", ignore_errors=True)
+    shutil.copytree("Basilisk_Sim.egg-info", "EGG-INFO")
+
+    with zipfile.ZipFile(root_pth / egg_name, "w") as zipf:
+        zipdir(Path("EGG-INFO"), zipf)
+        zipdir(Path("Basilisk"), zipf)
+
+    shutil.rmtree("EGG-INFO")
+
+    os.chdir(root_pth)
+
+    # ==========================================
+    # wheel file creation
+    # ==========================================
+    convert([egg_name], str(root_pth), False)
+    Path(egg_name).unlink()
+
+    for whl in root_pth.glob("*.whl"):
+        whl.rename(whl_name)
+
+
+if __name__ == "__main__":
+    build()
